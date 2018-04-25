d06650f88fb845571e3f88cf498af73e61efe52a
pidg0015 <38300482+pidg0015@users.noreply.github.com>
2018-04-25 15:27:54 -0400

    0425 afternoon

--- final/scripts/slider.js
+++ final/scripts/slider.js
@@ -1,11 +1,11 @@
-	let sliderImages = document.querySelectorAll(".slide"),
-  arrowLeft = document.querySelector("#arrow-left"),
-  arrowRight = document.querySelector("#arrow-right"),
-  current = 0;
+  var sliderImages = document.querySelectorAll(".slide");
+  var arrowLeft = document.querySelector("#arrow-left");
+  var arrowRight = document.querySelector("#arrow-right");
+  var current = 0;
 
 // Clear all images
 function reset() {
-  for (let i = 0; i < sliderImages.length; i++) {
+  for (var i = 0; i < sliderImages.length; i++) {
     sliderImages[i].style.display = "none";
   }
 }
