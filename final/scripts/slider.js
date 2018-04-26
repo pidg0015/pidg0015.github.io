43f2629f80578671488ab31b777326ce574ffc40
pidg0015 <38300482+pidg0015@users.noreply.github.com>
2018-04-25 20:11:44 -0400

    TADA

--- final/scripts/slider.js
+++ final/scripts/slider.js
@@ -3,34 +3,33 @@
   var arrowRight = document.querySelector("#arrow-right");
   var current = 0;
 
-// Clear all images
+
 function reset() {
   for (var i = 0; i < sliderImages.length; i++) {
     sliderImages[i].style.display = "none";
   }
 }
 
-// Init slider
+
 function startSlide() {
   reset();
   sliderImages[0].style.display = "block";
 }
 
-// Show prev
+
 function slideLeft() {
   reset();
   sliderImages[current - 1].style.display = "block";
   current--;
 }
 
-// Show next
-function slideRight() {
+
   reset();
   sliderImages[current + 1].style.display = "block";
   current++;
 }
 
-// Left arrow click
+
 arrowLeft.addEventListener("click", function() {
   if (current === 0) {
     current = sliderImages.length;
@@ -38,7 +37,7 @@ arrowLeft.addEventListener("click", function() {
   slideLeft();
 });
 
-// Right arrow click
+
 arrowRight.addEventListener("click", function() {
   if (current === sliderImages.length - 1) {
     current = -1;
